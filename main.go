77a994d1bdf32e4b7457fac43110ee80811f795d
blacktheangels <misyura.slava@mail.ru>
2024-10-25 23:18:11 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router:= mux.NewRouter()
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	http.ListenAndServe(":8080", router )
+
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+	log.Println("testing")
+}
