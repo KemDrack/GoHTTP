351237c32278397d0d184398e12744f0f56c8d4e
blacktheangels <misyura.slava@mail.ru>
2024-10-26 16:53:22 +0300

    при get запросе выводит Last received message: Hello World

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,15 +9,51 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var Message string = "Hello World" //  хранит последнее полученное сообщение , полученного от клиента
+
+
+type requestBody struct {
+	Message string `json:"message"` // хранения переданного JSON-поля "message"
+}
+
+
+
+func HandlerPost(w http.ResponseWriter, r *http.Request) { // ОТВЕТ ЗАПРОС  содержащую всю информацию о запросе (например, метод, заголовки, тело).
+	var requestBody requestBody
+
+	err:= json.NewDecoder(r.Body).Decode(&requestBody)
+	Message = requestBody.Message  
+
+	if err != nil {
+		http.Error(w, "Ошибка в JSON",http.StatusBadRequest)
+		return
+	}
+	log.Printf("Received message: %s", Message) 
+	fmt.Fprintf(w, "Message received: %s", Message)
+	
+}
+			
+
+func HandlerGET(w http.ResponseWriter , r *http.Request) {
+	if Message == "" {
+		http.Error(w, "Message not found" , http.StatusNotFound)
+		return
+	}
+	fmt.Fprintln(w, "Last received message:" , Message)
+}
+
+
+
 func main() {
-
+	// var Message int
 	router:= mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+
+	router.HandleFunc("/api/hello", HandlerGET).Methods("GET")
+	router.HandleFunc("/api/hello", HandlerPost).Methods("POST")
+	
+	log.Println("Server starting on :8080...")
 	http.ListenAndServe(":8080", router )
-
+	
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-	log.Println("testing")
-}
+
