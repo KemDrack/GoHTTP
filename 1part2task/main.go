9197bb97a796bf0408818a8de5814d84b68b05ab
blacktheangels <misyura.slava@mail.ru>
2024-10-27 19:36:24 +0300

    test

--- 1part2task/main.go
+++ 1part2task/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	
+	"encoding/json"
+	"fmt"
+
+	"log"
 	"net/http"
 
-	
 	"github.com/gorilla/mux"
-	
 )
 
+
+type requestBody struct {
+	Text string `json:"text"`
+}
+
+
+func PostMessage(w http.ResponseWriter, r *http.Request) {
+	var requestBody requestBody
+	err:= json.NewDecoder(r.Body).Decode(&requestBody)
+
+	if err!= nil {
+		http.Error(w, "Error in JSON", http.StatusBadRequest)
+		return
+	}
+	message:= Message{Text: requestBody.Text}
+
+	DB.Create(&message)
+	fmt.Fprintln(w, "Текст добавлен в БД")
+	
+}
+
+
+
+
+func GetMessages(w http.ResponseWriter, r *http.Request) {
+
+}
 
 
 func main() {
@@ -19,16 +47,10 @@ func main() {
 	DB.AutoMigrate(&Message{}) //  Gorm создаёт в базе данных таблицу с именем messages
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+
+	router.HandleFunc("/api/messages", PostMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
+
+	log.Println("Server is staring with DB on :8080 port...")
 	http.ListenAndServe(":8080", router)
-}
-
-func GetMessages(w http.ResponseWriter, r *http.Request) {
-
-}
-
-
-func CreateMessage(w http.ResponseWriter, r *http.Request) {
-
 }
